7eacf8be2d1f59f46dadfe75df801eb35f59aafa
mfacundo94 <mfacundo94@gmail.com>
2017-01-03 14:25:43 -0300

    segundo dia trabajo

--- backend.php
+++ backend.php
@@ -6,7 +6,9 @@
  * Time: 14:01
  */
 
-
+/*
+ * TODO : Verificar que lleguen bien los datos y no halla errores
+ */
 
 
 $mysqli = new mysqli("localhost", "root", "", "picadito");
@@ -28,13 +30,26 @@ $consulta = addslashes($_GET['consulta']);
 switch ($consulta)
 {
 
+    //TODO : NO TOMAR CAMPOS INSEGUROS
+    case "buscadorJugadores":
+        $campoBusqueda = addslashes($_GET["campoBusqueda"]);
+
+        $dbpet = $mysqli->query("SELECT * FROM usuarios_jugadores WHERE mail LIKE '%".$campoBusqueda."%' OR nombre LIKE '%".$campoBusqueda."%' LIMIT 0,5");
+        while($usuario = mysqli_fetch_array($dbpet, MYSQLI_ASSOC))
+        {
+            $jsonData[] = ($usuario);
+        }
+
+        echo json_encode($jsonData);
+        break;
+
     case "dameinfousuario":
         $dbpet = $mysqli->query("SELECT * FROM usuarios_jugadores WHERE mail='".$mail."' AND pass='".$pass."'");
         if (mysqli_num_rows($dbpet)<=0)
             die("invalidlogin");
         $usuario = mysqli_fetch_array($dbpet, MYSQLI_ASSOC);
 
-        echo json_encode($usuario);
+        echo "correcto:".json_encode($usuario);
         break;
 
 
@@ -46,7 +61,7 @@ switch ($consulta)
 
         $jsonData = array();
 
-        $dbpet = $mysqli->query("SELECT equipos.id_equipo, equipos.nombre, equipos.partidos_ganados, equipos.partidos_perdidos, equipos.partidos_empatados, equipos.partidos_suspendidos from equipos JOIN equipos_usuarios on equipos_usuarios.id_equipo = equipos.id_equipo WHERE equipos_usuarios.id_usuario=".$usuario["id_usuario"].";");
+        $dbpet = $mysqli->query("SELECT equipos.id_equipo, equipos.nombre, equipos.partidos_ganados, equipos.partidos_perdidos, equipos.partidos_empatados, equipos.partidos_suspendidos, equipos.fecha_alta, equipos.ultima_actividad, equipos.imagen from equipos JOIN equipos_usuarios on equipos_usuarios.id_equipo = equipos.id_equipo WHERE equipos_usuarios.id_usuario=".$usuario["id_usuario"].";");
         while($equipos = mysqli_fetch_array($dbpet, MYSQLI_ASSOC))
         {
             $jsonData[] = ($equipos);
